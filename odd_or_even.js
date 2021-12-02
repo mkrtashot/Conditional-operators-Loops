e8ebb3cc4db97d7ce125da0aca726590262b0c78
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-02 13:36:37 +0400

    added odd_or_even.js

---
+++ odd_or_even.js
@@ -0,0 +1,14 @@
+// Given a number. Print “odd” if the number is odd and “even” if itʼs even.
+// Input Output
+// 125 “odd”
+// 35 “odd”
+// 20 “even”
+
+let number = +prompt("Please enter your number");
+if (number == 0) {
+  console.log("0 is neither odd nor even");
+} else if (number % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
