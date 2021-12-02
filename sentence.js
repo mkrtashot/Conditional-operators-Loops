eaf29d5ec7dea0f6a292278e81da96ea54e12ecf
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-02 22:24:07 +0400

    added sentence.js

---
+++ sentence.js
@@ -0,0 +1,10 @@
+"use strict";
+// Given a sentence with missing words and an array of words. Replace all ‘_ʼ in a sentence with the words from the array.
+let arr1 = ["Houston", "problem"];
+let sentence1 = `${arr1[0]}, we have a ${arr1[1]}.`;
+
+let arr2 = ["first", "succeed", "again"];
+let sentence2 = `If at ${arr2[0]} you don't ${arr2[1]}, try, try ${arr2[2]}.`;
+
+let arr3 = ["Force", "be", "with", "you"];
+let sentence3 = `May the ${arr3[0]} ${arr3[1]} ${arr3[2]} ${arr3[3]}`;
