af0b506218e77ab98c7e6d0b6e4dfd3a9669af66
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-03 21:16:33 +0400

    fixed sentence.js

--- sentence.js
+++ sentence.js
@@ -1,10 +1,25 @@
 "use strict";
 // Given a sentence with missing words and an array of words. Replace all ‘_ʼ in a sentence with the words from the array.
+function sentence(sentnc, words) {
+  sentnc = sentnc.split("");
+  for (let i = 0; i < words.length; i++) {
+    for (let d = 0; d < sentnc.length; d++) {
+      if (sentnc[d] === "_") {
+        sentnc[d] = sentnc[d].replace("_", words[i]);
+        break;
+      }
+    }
+  }
+  return sentnc.join("");
+}
+
 let arr1 = ["Houston", "problem"];
-let sentence1 = `${arr1[0]}, we have a ${arr1[1]}.`;
+let sentence1 = "_, we have a _.";
 
 let arr2 = ["first", "succeed", "again"];
-let sentence2 = `If at ${arr2[0]} you don't ${arr2[1]}, try, try ${arr2[2]}.`;
+let sentence2 = `If at _ you don't _, try, try _.`;
 
 let arr3 = ["Force", "be", "with", "you"];
-let sentence3 = `May the ${arr3[0]} ${arr3[1]} ${arr3[2]} ${arr3[3]}`;
+let sentence3 = `May the _ _ _ _`;
+
+console.log(sentence(sentence3, arr3));
