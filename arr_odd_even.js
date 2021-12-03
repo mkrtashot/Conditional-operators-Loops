aff81bf8eb0bd0125c9cac0969704632830a4dff
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-03 12:06:07 +0400

    added strings_numbers.js, fixed arr_odd_even.js

--- arr_odd_even.js
+++ arr_odd_even.js
@@ -1,3 +1,6 @@
+"use strict";
+// Given mixed array of numbers, strings, booleans, nulls and undefined. Filter array and get all the numbers in a separate array. Arrange them such as from the beginning are the odds and from the ending the evens.
+
 function array(arr) {
   let newArr = [];
   let arrFinal = [];
